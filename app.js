f3f93ed825c7eb6d3047684edf57d58d6f934244
vishal6557 <diyora.v@husky.neu.edu>
2018-10-14 22:37:11 -0400

    bug fix

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(function (err, req, res, next) {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
     
+    console.log(err);
     // render the error page
     res.status(err.status || 200);
     res.send({ success: false, message: err });
--- routes/upload.js
+++ routes/upload.js
@@ -5,6 +5,23 @@ let UploadController = require('../controllers/upload');
 /**
  * Uplaod file to s3
  */
+router.post("/", async (req, res, next) => {
+
+    console.log("Coming in route")
+    try {
+
+        let message = await UploadController.upload(req);
+        res.json({
+            success: true,
+            message: message
+        })
+    }
+    catch (e) {
+        console.log("Error is here ",e);
+        next(e);
+    }
+})
+
 router.get("/", async (req, res, next) => {
 
     console.log("Coming in route get/")
@@ -18,7 +35,5 @@ router.get("/", async (req, res, next) => {
         next(e);
     }
 })
-
-
 
 module.exports = router;
